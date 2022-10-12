5157c09e0f920a4fa2a4c61d58d0b477c50f0626
Youlserf <youlserf.cardenas@tecsup.edu.pe>
2022-10-12 18:08:56 -0500

    Agregar archivos de proyecto.

---
+++ Lab7T/Lab7T/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Lab7T
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScannerQR_Clicked(object sender, EventArgs e) {
+            try {
+                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+                scanner.TopText = " Cardenas Youlserf";
+                scanner.BottomText = "Lab 07";
+                var result = await scanner.Scan();
+                if ( result != null) {
+                    txtResultado.Text = result.Text;
+                }
+            } catch(Exception ex) {
+                await DisplayAlert("Error", ex.Message.ToString(), "Ok");
+            }
+        }
+
+    }
+}
